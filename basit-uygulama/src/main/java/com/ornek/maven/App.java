5a0dfa5172ee141bcee17f6bbfa899ca9038aab8
aozturk <alparslan.ozturk@assistt.com.tr>
2022-05-23 15:21:10 +0300

    basit-uygulama eklendi

---
+++ basit-uygulama/src/main/java/com/ornek/maven/App.java
@@ -0,0 +1,13 @@
+package com.ornek.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
